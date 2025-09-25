b949cf9c14c9259c889d868fca7e3937959bb54e
Mikhail Romanov <mmromanov@ispras.ru>
2025-09-25 23:11:48 +0300

    02_Multifile done

--- 02_Multifile/prog.c
+++ 02_Multifile/prog.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]) {
               output("<DONE>");
      }
      else
-          usage(argv[0]);
+       //  usage(argv[0]);
+          usage("prog"); // for testing
        return 0;
 }
