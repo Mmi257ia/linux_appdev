6866e40b6aedd8ab3b41884d28922c347eed504b
Mikhail Romanov <mmromanov@ispras.ru>
2025-11-04 00:28:37 +0300

    add autotools templates

---
+++ 07_Environmental/remove_autotools_trash.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -rf autom4te.cache src/.deps
+rm -f src/.dirstamp src/rhasher.o
+rm -f aclocal.m4 compile config.h config.h.in config.log config.status configure depcomp install-sh Makefile Makefile.in missing stamp-h1
