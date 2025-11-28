047a1d32c52b6097c755b330943c540b5d49c06f
Mikhail Romanov <mmromanov@ispras.ru>
2025-11-29 00:50:24 +0300

    move function from header to .c

--- 10_LibTesting/buf.h
+++ 10_LibTesting/buf.h
@@ -80,33 +80,5 @@ struct buf {
 #define buf_clear(v) \
     ((v) ? (buf_ptr((v))->size = 0) : 0)
 
-
-static void *
-buf_grow1(void *v, size_t esize, ptrdiff_t n)
-{
-    struct buf *p;
-    size_t max = (size_t)-1 - sizeof(struct buf);
-    if (v) {
-        p = buf_ptr(v);
-        if (n > 0 && p->capacity + n > max / esize)
-            goto fail; /* overflow */
-        p = realloc(p, sizeof(struct buf) + esize * (p->capacity + n));
-        if (!p)
-            goto fail;
-        p->capacity += n;
-        if (p->size > p->capacity)
-            p->size = p->capacity;
-    } else {
-        if ((size_t)n > max / esize)
-            goto fail; /* overflow */
-        p = malloc(sizeof(struct buf) + esize * n);
-        if (!p)
-            goto fail;
-        p->capacity = n;
-        p->size = 0;
-    }
-    return p->buffer;
-fail:
-    BUF_ABORT;
-    return 0;
-}
+void *
+buf_grow1(void *v, size_t esize, ptrdiff_t n);
