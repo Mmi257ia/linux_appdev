1f1679e96c3cea27f21f469ed96547d110bb8c5c
Mikhail Romanov <mmromanov@ispras.ru>
2025-12-07 18:03:25 +0300

    add decimal-to-roman and vice versa conversion funcs

--- 11_Documenting/src/guesser.c
+++ 11_Documenting/src/guesser.c
@@ -27,6 +27,153 @@
 #define A 1
 #define B 100
 
+char *decimal_to_roman(int decimal) {
+    static char *roman[B - A + 1] = {
+        [1-A] = "I",
+        [2-A] = "II",
+        [3-A] = "III",
+        [4-A] = "IV",
+        [5-A] = "V",
+        [6-A] = "VI",
+        [7-A] = "VII",
+        [8-A] = "VIII",
+        [9-A] = "IX",
+        [10-A] = "X",
+        [11-A] = "XI",
+        [12-A] = "XII",
+        [13-A] = "XIII",
+        [14-A] = "XIV",
+        [15-A] = "XV",
+        [16-A] = "XVI",
+        [17-A] = "XVII",
+        [18-A] = "XVIII",
+        [19-A] = "XIX",
+        [20-A] = "XX",
+        [21-A] = "XXI",
+        [22-A] = "XXII",
+        [23-A] = "XXIII",
+        [24-A] = "XXIV",
+        [25-A] = "XXV",
+        [26-A] = "XXVI",
+        [27-A] = "XXVII",
+        [28-A] = "XXVIII",
+        [29-A] = "XXIX",
+        [30-A] = "XXX",
+        [31-A] = "XXXI",
+        [32-A] = "XXXII",
+        [33-A] = "XXXIII",
+        [34-A] = "XXXIV",
+        [35-A] = "XXXV",
+        [36-A] = "XXXVI",
+        [37-A] = "XXXVII",
+        [38-A] = "XXXVIII",
+        [39-A] = "XXXIX",
+        [40-A] = "XL",
+        [41-A] = "XLI",
+        [42-A] = "XLII",
+        [43-A] = "XLIII",
+        [44-A] = "XLIV",
+        [45-A] = "XLV",
+        [46-A] = "XLVI",
+        [47-A] = "XLVII",
+        [48-A] = "XLVIII",
+        [49-A] = "XLIX",
+        [50-A] = "L",
+        [51-A] = "LI",
+        [52-A] = "LII",
+        [53-A] = "LIII",
+        [54-A] = "LIV",
+        [55-A] = "LV",
+        [56-A] = "LVI",
+        [57-A] = "LVII",
+        [58-A] = "LVIII",
+        [59-A] = "LIX",
+        [60-A] = "LX",
+        [61-A] = "LXI",
+        [62-A] = "LXII",
+        [63-A] = "LXIII",
+        [64-A] = "LXIV",
+        [65-A] = "LXV",
+        [66-A] = "LXVI",
+        [67-A] = "LXVII",
+        [68-A] = "LXVIII",
+        [69-A] = "LXIX",
+        [70-A] = "LXX",
+        [71-A] = "LXXI",
+        [72-A] = "LXXII",
+        [73-A] = "LXXIII",
+        [74-A] = "LXXIV",
+        [75-A] = "LXXV",
+        [76-A] = "LXXVI",
+        [77-A] = "LXXVII",
+        [78-A] = "LXXVIII",
+        [79-A] = "LXXIX",
+        [80-A] = "LXXX",
+        [81-A] = "LXXXI",
+        [82-A] = "LXXXII",
+        [83-A] = "LXXXIII",
+        [84-A] = "LXXXIV",
+        [85-A] = "LXXXV",
+        [86-A] = "LXXXVI",
+        [87-A] = "LXXXVII",
+        [88-A] = "LXXXVIII",
+        [89-A] = "LXXXIX",
+        [90-A] = "XC",
+        [91-A] = "XCI",
+        [92-A] = "XCII",
+        [93-A] = "XCIII",
+        [94-A] = "XCIV",
+        [95-A] = "XCV",
+        [96-A] = "XCVI",
+        [97-A] = "XCVII",
+        [98-A] = "XCVIII",
+        [99-A] = "XCIX",
+        [100-A] = "C",
+    };
+    if (decimal > B || decimal < A) {
+        fprintf(stderr, "Number %d is too big to convert it to Roman\n", decimal);
+        return NULL;
+    }
+    return roman[decimal - A];
+}
+
+static int roman_digit_to_decimal(char roman) {
+    switch (roman) {
+        case 'C': return 100;
+        case 'L': return 50;
+        case 'X': return 10;
+        case 'V': return 5;
+        case 'I': return 1;
+        default:
+            fprintf(stderr, "Can't convert character %c from Roman to decimal number\n", roman);
+            return -1;
+    }
+}
+
+int roman_to_decimal(char *roman) {
+    int res = 0;
+    int i = 0;
+    int len = strlen(roman);
+    while (i < len) {
+        int cur_digit = roman_digit_to_decimal(roman[i]);
+        if (cur_digit < 0)
+            return -1;
+        if (i != len - 1) {
+            int next_digit = roman_digit_to_decimal(roman[i+1]);
+            if (cur_digit < 0)
+                return -1;
+            if (next_digit > cur_digit) {
+                res += next_digit - cur_digit;
+                i += 2;
+                continue;
+            }
+        }
+        res += cur_digit;
+        i += 1;
+    }
+    return res;
+}
+
 int main() {
     setlocale(LC_ALL, "");
     bindtextdomain(PACKAGE, LOCALE_PATH);
